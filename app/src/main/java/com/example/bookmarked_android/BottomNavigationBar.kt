ea34630bada6b5f5a01324384ffc6ebb7a9c4ea5
Jesica <jesica@btpn.com>
2024-06-13 10:09:16 +0700

    feat: add bottom navigation bar

--- app/src/main/java/com/example/bookmarked_android/BottomNavigationBar.kt
+++ app/src/main/java/com/example/bookmarked_android/BottomNavigationBar.kt
@@ -1,2 +1,51 @@
 package com.example.bookmarked_android
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookmarked_android.data.BottomNavigationItem
+
+val items = listOf(
+    BottomNavigationItem(title = "Home", icon = Icons.Rounded.Home),
+    BottomNavigationItem(title = "Bookmarks", icon = Icons.Rounded.List),
+    BottomNavigationItem(title = "Profile", icon = Icons.Rounded.Person)
+)
+@Preview
+@Composable
+fun BottomNavigationBar() {
+    NavigationBar {
+        Row(
+            modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = index == 0,
+                    onClick = { /*TODO*/ },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title,
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    },
+//                    label = {
+//                        Text(
+//                            text = item.title,
+//                            color = MaterialTheme.colorScheme.onBackground
+//                        )
+//                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bookmarked_android/components/SectionTitle.kt
@@ -0,0 +1,36 @@
+package com.example.bookmarked_android.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.sp
+
+class SectionTitleProvider : PreviewParameterProvider<String> {
+    override val values = sequenceOf("Section Title")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SectionTitlePreview(@PreviewParameter(SectionTitleProvider::class) title: String) {
+    SectionTitle(title)
+}
+
+@Composable
+fun SectionTitle(title: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = title, fontSize = 18.sp)
+        Text(text = "View more", fontSize = 14.sp, textDecoration = TextDecoration.Underline)
+    }
+}
