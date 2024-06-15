dcec098d507c667a872c9a8ae5ada34c916a7ef8
Jesica <jestsee@gmail.com>
2024-06-15 21:16:13 +0700

    feat: create custom card

--- app/src/main/java/com/example/bookmarked_android/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bookmarked_android/ui/screens/HomeScreen.kt
@@ -3,8 +3,9 @@ package com.example.bookmarked_android.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,10 +16,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bookmarked_android.ui.components.BottomNavigationBar
-import com.example.bookmarked_android.ui.components.SectionTitle
+import com.example.bookmarked_android.ui.components.RecentBookmarks
 
 @OptIn(ExperimentalMaterial3Api::class)
-//@Preview(showBackground = true)
 @Composable
 fun HomeScreen(bookmarkedUiState: BookmarkedUiState) {
     Scaffold(
@@ -26,22 +26,22 @@ fun HomeScreen(bookmarkedUiState: BookmarkedUiState) {
     ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding),
+                .padding(12.dp, 40.dp, 12.dp, innerPadding.calculateBottomPadding()),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             GreetingsText()
 //            TODO: search bar
-            SectionTitle(title = "Featured")
-            Card(onClick = { /*TODO*/ }) {
-                Text(text = "ini card")
-            }
-            SectionTitle(title = "Recently bookmarked")
-
+//            TODO: cards carousel
+//            SectionTitle(title = "Featured")
+//            Card(onClick = { /*TODO*/ }) {
+//                Text(text = "ini card")
+//            }
+            Spacer(modifier = Modifier.size(16.dp))
             when (bookmarkedUiState) {
                 is BookmarkedUiState.Error -> Text(text = "Error")
                 is BookmarkedUiState.Loading -> Text(text = "Loading...")
                 is BookmarkedUiState.Success -> {
-                    ResultScreen(photos = bookmarkedUiState.bookmarkedList.items.get(0).author)
+                    RecentBookmarks(bookmarkedUiState.bookmarkedList.items.take(5))
                 }
 
             }
