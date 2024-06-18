7cf6bbf695ba155b013d6766a9c532c2483b13b2
Jesica <jesica@btpn.com>
2024-06-18 07:08:29 +0700

    feat: add custom gson type adapter and styling detail screen

--- app/src/main/java/com/example/bookmarked_android/model/Content.kt
+++ app/src/main/java/com/example/bookmarked_android/model/Content.kt
@@ -2,16 +2,74 @@ package com.example.bookmarked_android.model
 
 
 import androidx.compose.runtime.Immutable
-import com.google.gson.annotations.SerializedName
+import com.google.gson.Gson
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+
+sealed class Content {
+    abstract val id: String
+    abstract val type: String
+}
 
 @Immutable
-data class Content(
-    @SerializedName("id")
-    val id: String,
-    @SerializedName("text")
+data class TextContent(
+    override val id: String,
+    override val type: String = "text",
     val text: String,
-    @SerializedName("type")
-    val type: String,
-    @SerializedName("url")
+    val url: String?
+) : Content()
+
+@Immutable
+data class ImageContent(
+    override val id: String,
+    override val type: String = "image",
     val url: String
-)
+) : Content()
+
+@Immutable
+data class CalloutContent(
+    override val id: String,
+    override val type: String = "callout",
+    val author: Author,
+    @Transient var contents: List<Content>,
+    val parentId: String,
+) : Content()
+
+class ContentTypeAdapter : JsonDeserializer<Content> {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): Content {
+        val jsonObject = json.getAsJsonObject()
+        val typeName = jsonObject.get("type").asString
+
+        return when (typeName) {
+            "text" -> Gson().fromJson(json, TextContent::class.java)
+            "image" -> Gson().fromJson(json, ImageContent::class.java)
+            "callout" -> {
+                var calloutContent = Gson().fromJson(json, CalloutContent::class.java)
+                val jsonContents = jsonObject.getAsJsonArray("contents")
+
+                val contents = jsonContents.map { jsonItem ->
+                    val itemType = jsonItem.asJsonObject.get("type").asString
+                    val itemId = jsonItem.asJsonObject.get("id").asString
+                    val itemText = jsonItem.asJsonObject.get("text")?.asString
+                    val itemUrl = jsonItem.asJsonObject.get("url")?.asString
+                    when (itemType) {
+                        "image" -> ImageContent(itemId, itemType, itemUrl!!)
+                        else -> TextContent(itemId, itemType, itemText!!, itemUrl)
+                    }
+                }
+                calloutContent.contents = contents
+                return calloutContent
+            }
+
+            else -> throw JsonParseException("Class not found during deserialization")
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bookmarked_android/ui/components/TextUrl.kt
@@ -0,0 +1,41 @@
+package com.example.bookmarked_android.ui.components
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextUrl(modifier: Modifier = Modifier, text: String, url: String) {
+    val uriHandler = LocalUriHandler.current
+    val annotatedText = buildAnnotatedString {
+        pushStringAnnotation(tag = "URL", annotation = url)
+
+        withStyle(
+            style = SpanStyle(
+                color = Color(0xFF8E85FF),
+                fontSize = 18.sp
+            )
+        ) { append(text) }
+        pop()
+    }
+
+    ClickableText(
+        modifier = modifier,
+        text = annotatedText,
+        onClick = { offset ->
+            annotatedText.getStringAnnotations(
+                tag = "URL",
+                start = offset,
+                end = offset
+            ).firstOrNull()?.let { annotation ->
+                uriHandler.openUri(annotation.item)
+            }
+        }
+    )
+}
