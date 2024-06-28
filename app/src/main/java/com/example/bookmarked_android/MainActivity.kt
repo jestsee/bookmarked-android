adcba41b60fe0a643179c27cfc38dce94e119f27
Jesica <jesica@btpn.com>
2024-06-28 23:51:36 +0700

    feat: hide bottom nav bar on detail image screen

--- app/src/main/java/com/example/bookmarked_android/MainActivity.kt
+++ app/src/main/java/com/example/bookmarked_android/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController
 import com.example.bookmarked_android.navigation.NavigationHost
 import com.example.bookmarked_android.ui.theme.BookmarkedandroidTheme
 
@@ -14,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BookmarkedandroidTheme {
-                NavigationHost(navController = rememberNavController())
+                NavigationHost()
             }
         }
     }
