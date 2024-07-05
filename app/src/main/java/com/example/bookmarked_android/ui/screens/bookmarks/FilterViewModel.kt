1aecfb886975ee370faee1bb25f1a6fe0936cd1c
Jesica <jesica@btpn.com>
2024-07-05 08:54:21 +0700

    feat: able to select and deselect type

---
+++ app/src/main/java/com/example/bookmarked_android/ui/screens/bookmarks/FilterViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.bookmarked_android.ui.screens.bookmarks
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+enum class BookmarkTypeEnum {
+    Tweet, Thread
+}
+
+val bookmarkTypes = BookmarkTypeEnum.entries.map { it }
+
+class FilterViewModel: ViewModel() {
+    private val _selectedType = MutableStateFlow<String?>(null)
+    val selectedType: StateFlow<String?> = _selectedType.asStateFlow()
+
+    fun selectType(type: BookmarkTypeEnum) {
+        _selectedType.value = type.name
+    }
+
+    fun deselectType() {
+        _selectedType.value = null
+    }
+}
