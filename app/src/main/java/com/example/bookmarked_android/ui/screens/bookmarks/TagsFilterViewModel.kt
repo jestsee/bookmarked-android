aa212425d21843bcc420a722f8430c38243aaf08
Jesica <jestsee@gmail.com>
2024-07-06 11:58:10 +0700

    feat: update scrollbar on tags dropdown

--- app/src/main/java/com/example/bookmarked_android/ui/screens/bookmarks/TagsFilterViewModel.kt
+++ app/src/main/java/com/example/bookmarked_android/ui/screens/bookmarks/TagsFilterViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.bookmarked_android.ui.screens.bookmarks
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bookmarked_android.Config
@@ -8,25 +9,41 @@ import com.example.bookmarked_android.network.NotionApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class TagsFilterViewModel : ViewModel() {
     private val config = Config()
 
-    private val _selectedTags = MutableStateFlow<List<Tag>>(emptyList())
-    val selectedTags: StateFlow<List<Tag>> = _selectedTags.asStateFlow()
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
 
-    private val _tagOptions = MutableStateFlow<List<Tag>>(emptyList())
+    private val allTags = MutableStateFlow(emptyList<Tag>())
+
+    private val _tagOptions = MutableStateFlow(emptyList<Tag>())
     val tagOptions: StateFlow<List<Tag>> = _tagOptions.asStateFlow()
 
+    private val _selectedTags = MutableStateFlow(mutableListOf<Tag>())
+    val selectedTags: StateFlow<List<Tag>> = _selectedTags.asStateFlow()
+
     private val _error = MutableStateFlow<String?>(null)
-    val error: StateFlow<String?> = _error.asStateFlow()
+    val error: StateFlow<String?> = _error.asStateFlow() // TODO
 
     private val _isLoading = MutableStateFlow<Boolean>(false)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
     init {
         fetchTags()
+        observeQuery()
+    }
+
+    private fun observeQuery() {
+        viewModelScope.launch {
+            _searchQuery.collectLatest {
+                _tagOptions.value =
+                    allTags.value.filter { tag -> tag.name.contains(it, ignoreCase = true) }
+            }
+        }
     }
 
     private fun fetchTags() {
@@ -39,6 +56,7 @@ class TagsFilterViewModel : ViewModel() {
                     "Bearer ${config.notionSecret}",
                     config.databaseId
                 )
+                allTags.value = response.results
                 _tagOptions.value = response.results
             } catch (e: Exception) {
                 _error.value = e.message
@@ -46,5 +64,22 @@ class TagsFilterViewModel : ViewModel() {
                 _isLoading.value = false
             }
         }
+    }
+
+    fun selectTag(tag: Tag) {
+        _selectedTags.value.add(tag)
+    }
+
+    fun deselectTag(tag: Tag) {
+        Log.d("TAG", "deselectTag: masok ${tag.name}")
+        _selectedTags.value.remove(tag)
+    }
+
+    fun deselectAllTags() {
+        _selectedTags.value.clear()
+    }
+
+    fun searchTags(query: String) {
+        _searchQuery.value = query
     }
 }
--- app/src/main/java/com/example/bookmarked_android/utils/scrollbar.kt
+++ app/src/main/java/com/example/bookmarked_android/utils/scrollbar.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -20,8 +21,11 @@ fun Modifier.verticalScrollBar(
     ratio: Float = 3f,
     width: Dp = 8.dp
 ): Modifier {
-    val targetAlpha = if (state.isScrollInProgress) 1f else 0f
+    val targetAlpha = if (state.isScrollInProgress) 1f else .5f
     val duration = if (state.isScrollInProgress) 150 else 500
+
+    val horizontalOffset = 4.dp
+    val verticalOffset = 6.dp
 
     val alpha by animateFloatAsState(
         targetValue = targetAlpha,
@@ -31,16 +35,18 @@ fun Modifier.verticalScrollBar(
     return drawWithContent {
         drawContent()
 
-        val needDrawScrollbar = state.isScrollInProgress || alpha > 0.0f
         val barHeight = (this.size.height / ratio)
         val barRange = (this.size.height - barHeight) / state.maxValue
-        if (needDrawScrollbar) {
-            val position = state.value * barRange
-            drawRect(
-                color = color.copy(alpha = alpha),
-                topLeft = Offset(this.size.width - width.toPx(), position),
-                size = Size(width.toPx(), barHeight)
-            )
-        }
+        val position = state.value * barRange
+
+        drawRoundRect(
+            color = color.copy(alpha),
+            topLeft = Offset(
+                this.size.width - width.toPx() - horizontalOffset.toPx(),
+                position + verticalOffset.toPx()
+            ),
+            cornerRadius = CornerRadius(x = 25f, y = 25f),
+            size = Size(width.toPx(), barHeight - 1.5f * verticalOffset.toPx()),
+        )
     }
 }
